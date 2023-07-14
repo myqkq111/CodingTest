9255e004cf1f0a0aab04f5509b3aa7cfbac2265d
82107 <82107@192.168.219.104>
2023-07-15 04:25:16 +0900

    CodingTest

--- src/level_0/Level_0.java
+++ src/level_0/Level_0.java
@@ -5,11 +5,11 @@ public class Level_0 {
 	public static void main(String[] args) {
 		
 		//메서드 클래스 객체생성
-
 		Level_0_Method lm = new Level_0_Method();						
 
 		
 
+		
 		//1. 정수 a와 b가 주어집니다. 각 수를 입력받아 입출력 예와 같은 형식으로 출력하는 코드를 작성해 보세요.
 
 //			Scanner sc = new Scanner(System.in);
@@ -172,7 +172,105 @@ public class Level_0 {
 		 * 단, 시작할 때 mode는 0이며, return 하려는 ret가 만약 빈 문자열이라면 대신 "EMPTY"를 return 합니다.
 		 */
 		
-//			System.out.println(lm.solution("qasdas"));
+//		System.out.println(lm.solution("qasdas"));
+		
+		
+		
+		
+		
+		//---------------------------------------------------------------------------------
+		
+		
+		
+		
+		
+		/* 9. 정수 배열 numLog가 주어집니다. 처음에 numLog[0]에서 부터 시작해 "w", "a", "s", "d"로 이루어진 문자열을
+		 * 입력으로 받아 순서대로 다음과 같은 조작을 했다고 합시다.
+		 * 
+		 * "w" : 수에 1을 더한다. "s" : 수에 1을 뺀다. "d" : 수에 10을 더한다. "a" : 수에 10을 뺀다. 그리고 매번
+		 * 조작을 할 때마다 결괏값을 기록한 정수 배열이 numLog입니다. 즉, numLog[i]는 numLog[0]로부터 총 i번의 조작을 가한
+		 * 결과가 저장되어 있습니다.
+		 * 
+		 * 주어진 정수 배열 numLog에 대해 조작을 위해 입력받은 문자열을 return 하는 solution 함수를 완성해 주세요.
+		 */
+		
+//		int[] numLog = {0,1,0,10,0,1,0,10,0,-1,-2,-1,0};  
+//		System.out.println(lm.solution(numLog));
+		
+		
+		
+		
+		
+		//---------------------------------------------------------------------------------
+		
+		
+		
+		
+		/*
+		 * 10. 정수 배열 arr와 2차원 정수 배열 queries이 주어집니다. queries의 원소는 각각 하나의 query를 나타내며, [i,
+		 * j] 꼴입니다.
+		 * 
+		 * 각 query마다 순서대로 arr[i]의 값과 arr[j]의 값을 서로 바꿉니다.
+		 * 
+		 * 위 규칙에 따라 queries를 처리한 이후의 arr를 return 하는 solution 함수를 완성해 주세요.
+		 */
+		
+//		int[] arr = {0, 1, 2, 3, 4}; 
+//		int[][] queries = {
+//				{0,3}, {1,2}, {1,4}
+//				};
+//		
+//		int[] res = lm.solution(arr,queries);
+//		for (int is : res) {
+//			System.out.println(is);
+//		}
+		
+		
+		
+		
+		
+		
+		//---------------------------------------------------------------------------------
+		
+		
+		
+		
+		
+		
+		
+		/*
+		 * 11. 정수 배열 arr와 2차원 정수 배열 queries이 주어집니다. queries의 원소는 각각 하나의 query를 나타내며, [s,
+		 * e, k] 꼴입니다.
+		 * 
+		 * 각 query마다 순서대로 s ≤ i ≤ e인 모든 i에 대해 k보다 크면서 가장 작은 arr[i]를 찾습니다.
+		 * 
+		 * 각 쿼리의 순서에 맞게 답을 저장한 배열을 반환하는 solution 함수를 완성해 주세요. 단, 특정 쿼리의 답이 존재하지 않으면 -1을
+		 * 저장합니다.
+		 */
+				
+//		int[] arr = {0, 1, 2, 4, 3}; 
+//		int[][] queries = {
+//				{0,4,2}, {0,3,2}, {0,2,2}
+//				};
+//		
+//		int[] res = lm.solution(arr,queries);
+//		
+//		for (int is : res) {
+//			System.out.println(is);
+//		}
+		
+		
+		
+		
+		
+		
+		//---------------------------------------------------------------------------------
+		
+		
+		
+		
+		
+		
 		
 		
 		
--- src/level_0/Level_0_Method.java
+++ src/level_0/Level_0_Method.java
@@ -3,12 +3,14 @@ package level_0;
 public class Level_0_Method {
 	
 		//4번
-		public String solution(String my_string, String overwrite_string, int s) {
-		
-			String a = my_string.substring(s, s+overwrite_string.length());
-			String[] b = my_string.split(a);			
-
-		}
+		/*
+		 * public String solution(String my_string, String overwrite_string, int s) {
+		 * 
+		 * String a = my_string.substring(s, s+overwrite_string.length()); String[] b =
+		 * my_string.split(a);
+		 * 
+		 * }
+		 */
 
 
 		//---------------------------------------------------------------------------------
@@ -131,47 +133,172 @@ public class Level_0_Method {
 		
 		
 
-				//8번
-//				public String solution(String code) {
+			//8번
+//			public String solution(String code) {
 //					
-//					String mode = "0";
-//					String ret = "";
+//				String mode = "0";
+//				String ret = "";
 //					
-//					for(int i = 0; i < code.length(); i++) {
-//						switch(mode) {
+//				for(int i = 0; i < code.length(); i++) {
+//					switch(mode) {
 //						
-//						case "0":
+//					case "0":
 //							
-//							if(code.charAt(i) == '1') {
-//								mode = "1";
-//							}else {
-//								if(i % 2 == 0) {
-//									ret += code.charAt(i);
-//								}
+//						if(code.charAt(i) == '1') {
+//							mode = "1";
+//						}else {
+//							if(i % 2 == 0) {
+//								ret += code.charAt(i);
 //							}
+//						}
 //							
-//							break;		
+//						break;		
 //							
-//						case "1":
+//					case "1":
 //							
-//							if(code.charAt(i) == '1') {
-//								mode = "0";
-//							}else {
-//								if(i % 2 == 1) {
-//									ret += code.charAt(i);
-//								}
+//						if(code.charAt(i) == '1') {
+//							mode = "0";
+//						}else {
+//							if(i % 2 == 1) {
+//								ret += code.charAt(i);
 //							}
+//						}
 //							
-//							break;
-//						}//switch				
-//					}//for
+//						break;
+//					}//switch				
+//				}//for
 //					
-//					if(ret == "") {
-//						return "EMPTY";
-//					}else {
-//						return ret;	
-//					}
-//						
+//				if(ret == "") {
+//					return "EMPTY";
+//				}else {
+//					return ret;	
 //				}
+//						
+//			}
+	
+	
+	
+	
+	
+	//---------------------------------------------------------------------------------
+	
+	
+		
+	
+	
+		//9번
+//		public String solution(int[] numLog) {
+//			
+//			String answer = "";
+//			//현재 인덱스와 전 인덱스의 값을 빼서 나온 값에 따라 answer변수에 알맞는 알파벳을 넣어준다.
+//			for(int i = 1; i < numLog.length; i++) {	
+//				if(numLog[i] - numLog[i-1] == 1) {
+//					answer += "w";
+//				}else if (numLog[i] - numLog[i-1] == -1) {
+//					answer += "s";
+//				}else if(numLog[i] - numLog[i-1] == 10) {
+//					answer += "d";
+//				}else {
+//					answer += "a";
+//				}
+//			}//for
+//			
+//			return answer;
+//			
+//		}
+	
+	
+	
+	
+	
+	//---------------------------------------------------------------------------------
+	
+	
+	
+	
+	
+	
+		//10번
+//		public int[] solution(int[] arr, int[][] queries) {
+//	        
+//			int idx1;
+//			int idx2;
+//			int su;
+//			
+//			//2차원 배열의 i마다 들어있는 값들을 변수에 담아주고 
+//			for(int i = 0; i < queries.length; i++) {
+//				idx1 = queries[i][0];
+//				idx2 = queries[i][1];
+//				
+//				//담아준 변수값과 arr변수의 인덱스에 해당하는 값을 서로 바꿔주는 작업
+//				su = arr[idx1];
+//				arr[idx1] = arr[idx2];
+//				arr[idx2] = su;
+//			}
+//			
+//			return arr;
+//			
+//	    }
+	
+	
+	
+	
+	
+	
+	//---------------------------------------------------------------------------------
+	
+	
+	
+	
+	
+	
+		//11번
+//		public int[] solution(int[] arr, int[][] queries) {
+//	        
+//			int s, e, k;
+//			int[] answer = new int[queries.length];
+//			
+//			
+//			for(int i = 0; i < queries.length; i++) {
+//				s = queries[i][0]; //arr[]의 시작인덱스
+//				e = queries[i][1]; //arr[]의 끝인덱스
+//				k = queries[i][2];
+//				
+//				for(int j = s; j <= e; j++) { 
+//					if(arr[j] > k) { //k값보다 클경우만
+//						if(answer[i] == 0) { // int는 값이 아무것도 없을 때 0을 반환한다.를 이용하여 처음 등록했을땐 그냥 등록하기
+//							answer[i] = arr[j];
+//						}else { //처음 등록하는게 아니라면
+//							if(answer[i] >= arr[j]) { //원래있던 값과 새로 넣을 값을 비교하여 새로 넣을 값이 더 작을 경우
+//								answer[i] = arr[j]; //새로운 값으로 바꾼다.
+//							}else { //원래있던값이 더 작을 경우
+//								continue; //가장 가까운 증감식으로 이동.(원래있던 값을 그대로 가져간다.)
+//							}
+//						}					 
+//					}				
+//				}
+//				if(answer[i] == 0) { //반복문을 다 돌았을 때 answer[i]에 값이 아무것도 없을 경우 -1을 넣는다.
+//					answer[i] = -1;
+//				}
+//			}
+//			
+//			return answer;
+//			
+//	    }
+	
+	
+	
+	
+	
+	//---------------------------------------------------------------------------------
+	
+	
+	
+	
+	
+	
+		
+	
+	
 
 }
