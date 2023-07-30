51cf6a579976ac6604e5501d3b93c616de763b6a
myqkq111 <myqkq111@naver.com>
2023-07-31 04:20:33 +0900

    CodingTest

---
+++ src/level_0_introduction/Level_0_Introduction.java
@@ -0,0 +1,92 @@
+package level_0_introduction;
+
+import java.util.Arrays;
+
+public class Level_0_Introduction {
+	public static void main(String[] args) {
+		
+		Level_0_Introduction_Method lim = new Level_0_Introduction_Method();
+		
+//		1. 등차수열 혹은 등비수열 common이 매개변수로 주어질 때, 마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요.
+//		제한사항
+//		2 < common의 길이 < 1,000
+//		-1,000 < common의 원소 < 2,000
+//		common의 원소는 모두 정수입니다.
+//		등차수열 혹은 등비수열이 아닌 경우는 없습니다.
+//		등비수열인 경우 공비는 0이 아닌 정수입니다.
+		
+//		int[] common = {3,9,27};
+//		
+//		System.out.println(lim.solution(common));
+		
+		
+		
+		
+		
+		
+	//--------------------------------------------------------------------------
+		
+		
+		
+		
+		
+		
+		
+//		2. 연속된 세 개의 정수를 더해 12가 되는 경우는 3, 4, 5입니다. 두 정수 num과 total이 주어집니다. 
+//		연속된 수 num개를 더한 값이 total이 될 때, 정수 배열을 오름차순으로 담아 return하도록 solution함수를 완성해보세요.
+//
+//		제한사항
+//		1 ≤ num ≤ 100
+//		0 ≤ total ≤ 1000
+//		num개의 연속된 수를 더하여 total이 될 수 없는 테스트 케이스는 없습니다.
+		
+//		int num = 99;
+//		int total = 0;
+//		
+//		System.out.println(Arrays.toString(lim.solution(num, total)));
+		
+		
+		
+		
+		
+		
+		//--------------------------------------------------------------------------
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}//main	
+}
---
+++ src/level_0_introduction/Level_0_Introduction_Method.java
@@ -0,0 +1,109 @@
+package level_0_introduction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Level_0_Introduction_Method {
+	
+	//Level 0 입문
+	
+	//1번
+//	public int solution(int[] common) {
+//		
+//		int res; //결과값을 담을 변수
+//		
+//		if(common[0] - common[1] == common[1] - common[2]) { //등차수열인지 확인
+//			int a = common[1] - common[0]; //등차수열이 맞다면 공차를 구하고
+//			res = common[common.length-1] + a; //마지막원소 + 공차
+//		} else { //등차수열이 아니라면
+//			int a = common[1] / common[0]; //공비를 구하고
+//			res = common[common.length-1] * a; //마지막원소 * 공비
+//		}
+//		return res;
+//	}
+	
+	
+	
+	
+	
+	
+	
+	//--------------------------------------------------------------------------
+	
+	
+	
+	
+	
+	
+	
+	//2번
+//	public int[] solution(int num, int total) {
+//		
+//		int[] res = new int[num]; //값을 담을 배열
+//		
+//		for(int i = -50; i <= 1000; i++) { // num의 최대값인 100과 total의 최대값인 1000을 이용해 모든 경우의 수를 계산했을때
+//										   // 나올수 있는 원소가 -50부터 1000이기 때문에 반복문을 통해 돌려준다.			
+//			int idx = 0; //인덱스값으로 사용
+//			
+//			for(int j = i; j < i+num; j++) { //-50부터 순차적으로 num의 갯수만큼 배열에 담고
+//				res[idx] = j;
+//				idx++;
+//			}			
+//			
+//			int sum = 0;
+//			
+//			idx = 0;
+//			
+//			for(int j = 0; j < res.length; j++) { //배열에 담긴 원소의 합이 total인지를 확인한다.
+//				sum+=res[idx];
+//				idx++;
+//			}
+//			
+//			if(sum == total) { //원소의 합이 total이라면
+//				break; //반복문 종료후 배열 반환, 아니라면 다음 원소로 넘어간다.
+//			}
+//		}
+//		return res;
+//	}
+
+	
+	
+	
+	
+	
+	
+	//--------------------------------------------------------------------------
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
