95d0be36068c5676f9d00b34a46d56da00b3e8ca
최준서 <myqkq111@naver.com>
2023-11-09 23:59:35 +0900

    CodingTest

--- src/level_1/Level_1_Method.java
+++ src/level_1/Level_1_Method.java
@@ -2023,6 +2023,51 @@ public class Level_1_Method {
 	
 	
 	
+	//46. Çà·ÄÀÇ µ¡¼À
+//	public int[][] solution(int[][] arr1, int[][] arr2){
+//		
+//		int[][] res = new int[arr1.length][arr1[0].length];
+//		
+//		for(int i = 0; i < arr1.length; i++) {
+//			for(int j = 0; j < arr1[i].length; j++) {
+//				res[i][j] = arr1[i][j] + arr2[i][j];
+//			}
+//		}
+//		return res;
+//	}
+
+	
+	
+	
+	
+	
+	//--------------------------------------------------------------------------
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
