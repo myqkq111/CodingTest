a76265f1e604849b0f731262375fa15d078a07e0
최준서 <myqkq111@naver.com>
2023-11-30 00:13:24 +0900

    CodingTest

--- src/level_1/Level_1_Method.java
+++ src/level_1/Level_1_Method.java
@@ -2705,6 +2705,40 @@ public class Level_1_Method {
 	
 	
 	//--------------------------------------------------------------------------
+	
+	
+	
+	
+	
+	//70. 나누어 떨어지는 숫자 배열
+//	public int[] solution(int[] arr, int divisor) {
+//		
+//		List<Integer> list = new ArrayList<Integer>();
+//		
+//		for(int i = 0; i < arr.length; i++) {
+//			if(arr[i] % divisor == 0) {
+//				list.add(arr[i]);
+//			}
+//		}
+//		if(list.size() == 0) {
+//			list.add(-1);
+//		}
+//		
+//		int[] res = new int[list.size()];
+//		
+//		for(int i = 0; i < list.size(); i++) {
+//			res[i] = list.get(i);
+//		}
+//		Arrays.sort(res);
+//		return res;
+//	}
+
+	
+	
+	
+	
+	
+	//--------------------------------------------------------------------------
 
 	
 	
