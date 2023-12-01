73291451fab40c59971f971f8875390aa57c6c5b
최준서 <myqkq111@naver.com>
2023-12-01 23:57:40 +0900

    CodingTest

--- src/level_1/Level_1_Method.java
+++ src/level_1/Level_1_Method.java
@@ -2739,6 +2739,37 @@ public class Level_1_Method {
 	
 	
 	//--------------------------------------------------------------------------
+	
+	
+	
+	
+	
+	//71. 같은 숫자는 싫어
+//	public int[] solution(int[] arr) {
+//		
+//		List<Integer> list = new ArrayList<Integer>();
+//		list.add(arr[0]);
+//		
+//		for(int i = 1; i < arr.length; i++) {
+//			if(arr[i] != arr[i-1]) {
+//				list.add(arr[i]);
+//			}
+//		}
+//		
+//		int[] res = new int[list.size()];
+//		
+//		for(int i = 0; i < list.size(); i++) {
+//			res[i] = list.get(i);
+//		}
+//		return res;
+//	}
+
+	
+	
+	
+	
+	
+	//--------------------------------------------------------------------------
 
 	
 	
