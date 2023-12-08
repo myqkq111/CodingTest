6e8a4028687ed046d9da52341db6a1b8d88d1a71
최준서 <myqkq111@naver.com>
2023-12-08 19:23:48 +0900

    CodingTest

---
+++ src/level_2/Level_2_Method.java
@@ -0,0 +1,48 @@
+package level_2;
+
+import java.util.Arrays;
+
+public class Level_2_Method {
+	
+	//1. ÃÖ´ñ°ª°ú ÃÖ¼Ú°ª
+//	public String solution(String s) {
+//		
+//		String[] str = s.split(" ");
+//		int[] intr = new int[str.length];
+//		
+//		for(int i = 0; i < str.length; i++) {
+//			intr[i] = Integer.parseInt(str[i]);
+//		}
+//		Arrays.sort(intr);
+//		
+//		String res = intr[0] + " " + intr[intr.length-1];
+//		
+//		return res;
+//	}
+	
+	
+	
+	
+	
+	
+	//--------------------------------------------------------------------------
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
